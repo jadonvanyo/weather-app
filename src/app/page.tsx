9f6079421667e3af1ba4408833dbb877c6226b02
jayman779 <jadon.vanyo@gmail.com>
2024-09-26 14:18:10 -0400

    feat: added hourly weather updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { format, parse, parseISO } from "date-fns";
+import Container from "@/components/Container";
+import { convertKelvinToFahrenheit } from "@/utils/convertKelvinToFahrenheit";
 
 type WeatherData = {
   cod: string;
@@ -74,7 +76,7 @@ export default function Home() {
 
   const firstData = data?.list[0];
 
-  console.log("data", firstData?.dt_txt)
+  console.log("data", firstData)
 
   if (isLoading) 
     return (
@@ -88,12 +90,46 @@ export default function Home() {
       <Navbar />
       {/* today data */}
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4 text-black">
-        <section>
-          <div>
+        <section className="space-y-4">
+          <div className="space-y-2">
             <h2 className="flex gap-1 text-2xl items-end">
               <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
               <p className="text-lg">({format(parseISO(firstData?.dt_txt ?? ""), "MM.dd.yyyy")})</p>
             </h2>
+            <Container className="gap- 10 px-6 items-center">
+              {/* temperature */}
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertKelvinToFahrenheit(firstData?.main.temp ?? 297)}°
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels like</span>
+                  <span>
+                    {convertKelvinToFahrenheit(firstData?.main.feels_like ?? 297)}°
+                  </span>
+                </p>
+                <p className="text-xs space-x-2 whitespace-nowrap">
+                  <span>
+                    H: {convertKelvinToFahrenheit(firstData?.main.temp_max ?? 0)}°
+                  </span>
+                  <span>
+                    L: {convertKelvinToFahrenheit(firstData?.main.temp_min ?? 0)}°
+                  </span>
+                </p>
+              </div>
+              {/* time and weather icon */}
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                {data?.list.map((d, i) => (
+                  <div
+                    key={i}
+                    className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"
+                  >
+                    <p className="whitespace-nowrap">{format(parseISO(d.dt_txt), "h:mm a")}</p>
+                    <p>{convertKelvinToFahrenheit(d?.main.temp ?? 0)}°</p>
+                  </div>
+                ))}
+              </div>
+            </Container>
           </div>
         </section>
       </main>
